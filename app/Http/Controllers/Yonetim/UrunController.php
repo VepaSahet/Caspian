b20b92c97ae50e79d4a08e3fc5ae7af44afa5d57
VepaSahet <mrvepa50@gmail.com>
2021-02-11 17:52:40 +0500

    128.ders başarılı bitti

---
+++ app/Http/Controllers/Yonetim/UrunController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Yonetim;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Urun;
+
+class UrunController extends Controller
+{
+    public function index()
+    {
+        if (request()->filled('aranan')) {
+            request()->flash();
+            $aranan = request('aranan');
+            $list = Urun::where('urun_adi','like', "%$aranan%")
+                ->orWhere('aciklama', 'like', "%$aranan%")
+                ->orderByDesc('id')
+                ->paginate(8)
+                ->appends('aranan', $aranan);
+
+        }
+        else {
+            $list = Urun::orderByDesc('id')->paginate(8);
+        }
+        return view('yonetim.urun.index', compact('list'));
+    }
+
+    public  function form($id = 0)
+    {
+        $entry = new Urun;
+        if ($id > 0){
+            $entry = Urun::find($id);
+        }
+        return view('yonetim.urun.form', compact('entry'));
+    }
+
+    public function kaydet($id = 0)
+    {
+        $data = request()->only('urun_adi', 'slug', 'aciklama', 'fiyati');
+        if (!request()->filled('slug')){
+            $data['slug']= str_slug(request('urun_adi'));
+            request()->merge(['slug'=> $data['slug']]);
+        }
+
+        $this->validate(request(),[
+            'urun_adi'  =>'required',
+            'fiyati'  =>'required',
+            'slug'          =>(request('original_slug') != request('slug') ? 'unique:urun,slug' : '')
+        ]);
+
+        $data_detay = request()->only('goster_slider',
+            'goster_gunun_firsati', 'goster_one_cikan', 'goster_cok_satan', 'goster_indirimli');
+
+        if ($id>0) {
+            $entry = Urun::where('id', $id)->firstOrFail();
+            $entry->update($data);
+
+            $entry->detay()->update($data_detay);
+
+        } else {
+            $entry = Urun::create($data);
+            $entry->detay()->create($data_detay);
+        }
+
+        return redirect()
+            ->route('yonetim.urun.duzenle', $entry->id)
+            ->with('mesaj', ($id > 0 ? 'Güncellendi' : 'Kaydedildi'))
+            ->with('mesaj_tur','success');
+    }
+
+    public function sil($id)
+    {
+        Kullanici::destroy($id);
+
+        return redirect()
+            ->route('yonetim.kullanici')
+            ->with('mesaj','Kayıt silindi')
+            ->with('mesaj_tur', 'success');
+    }
+}
--- resources/views/yonetim/layouts/partials/sidebar.blade.php
+++ resources/views/yonetim/layouts/partials/sidebar.blade.php
@@ -2,21 +2,40 @@
     <a href="{{ route('yonetim.anasayfa') }}" class="list-group-item">
         <span class="fa fa-fw fa-dashboard"></span> Giriş
     </a>
-    <a href="#" class="list-group-item">
-        <span class="fa fa-fw fa-dashboard"></span> Ürünler
+    <a href="{{ route('yonetim.urun') }}" class="list-group-item">
+        <span class="fa fa-fw fa-cubes"></span> Ürünler
         <span class="badge badge-dark badge-pill pull-right">14</span>
     </a>
-    <a href="{{ route('yonetim.kategori') }}" class="list-group-item collapsed" data-target="#submenu1" data-toggle="collapse" data-parent="#sidebar"><span class="fa fa-fw fa-dashboard"></span>Kategoriler<span class="caret arrow"></span></a>
+    <a href="{{ route('yonetim.kategori') }}" class="list-group-item">
+        <span class="fa fa-fw fa-folder"></span> Kategoriler
+        <span class="badge badge-dark badge-pill pull-right">14</span>
+    </a>
+    <a href="#" class="list-group-item">
+        <span class="fa fa-fw fa-comment"></span> Ürün Yorumlar
+        <span class="badge badge-dark badge-pill pull-right">14</span>
+    </a>
+    <a href="#" class="list-group-item collapsed" data-target="#submenu1" data-toggle="collapse"
+       data-parent="#sidebar"><span class="fa fa-fw fa-folder"></span> Kategori Ürünleri <span class="caret arrow"></span></a>
     <div class="list-group collapse" id="submenu1">
         <a href="#" class="list-group-item">Categories</a>
         <a href="#" class="list-group-item">Categories</a>
     </div>
     <a href="{{ route('yonetim.kullanici') }}" class="list-group-item">
-        <span class="fa fa-fw fa-dashboard"></span> Kullanıcılar
+        <span class="fa fa-fw fa-users"></span> Kullanıcılar
         <span class="badge badge-dark badge-pill pull-right">14</span>
     </a>
     <a href="#" class="list-group-item">
-        <span class="fa fa-fw fa-dashboard"></span> Siparişler
+        <span class="fa fa-fw fa-shopping-cart"></span> Siparişler
+        <span class="badge badge-dark badge-pill pull-right">14</span>
+    </a>
+    <a href="#" class="list-group-item collapsed" data-target="#submenu2" data-toggle="collapse"
+       data-parent="#sidebar"><span class="fa fa-fw fa-percent"></span> Raporlar <span class="caret arrow"></span></a>
+    <div class="list-group collapse" id="submenu2">
+        <a href="#" class="list-group-item"> Çok Satan Ürünler </a>
+        <a href="#" class="list-group-item"> Günlere Göre Satışlar </a>
+    </div>
+    <a href="#" class="list-group-item">
+        <span class="fa fa-fw fa-adjust "></span> Site Ayarlar
         <span class="badge badge-dark badge-pill pull-right">14</span>
     </a>
 </div>
